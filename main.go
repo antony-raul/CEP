09b14ba0b35592b6de6c13e1fa684c5fec2748d2
antony-raul <raulzin.raul@gmail.com>
2022-10-12 22:41:09 -0300

    rota de busca pelo cep.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/antony-raul/CEP/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
+	r.GET("buscar/endereco/:cep", controllers.BuscaEnderecoPeloCep)
 	r.Run()
 }
